ffbe70eaa626fcad6fb4d5cd18e697eeb5df05f2
Todd Flanders <todd.a.flanders@accenture.com>
2020-01-30 21:01:17 -0500

    pairing with jake

--- src/roman.cpp
+++ src/roman.cpp
@@ -1,5 +1,28 @@
 #include "roman.hpp"
 
 string toRoman(uint16_t n) {
-    return "I";
+   
+   string result = "";
+
+   while (n >= 10) {
+       result.append("X");
+       n -= 10;
+   }
+
+   while(n >= 5) {
+       result.append("V");
+       n -= 5;
+   }
+
+    while ( n >=4){
+        result.append("IV");
+        n-=4;
+    }
+
+   while (n >= 1) {
+       result.append("I");
+       n -= 1;
+   }
+
+    return result;
 }
--- test/test_roman.cpp
+++ test/test_roman.cpp
@@ -5,3 +5,35 @@ TEST(ToRoman, one) {
 
     ASSERT_EQ("I", toRoman(1));
 }
+
+TEST(ToRoman, two) {
+
+    ASSERT_EQ("II", toRoman(2));
+}
+
+TEST(ToRoman, three) {
+
+    ASSERT_EQ("III", toRoman(3));
+}
+
+TEST(ToRoman, four){
+
+    ASSERT_EQ("IV", toRoman(4));
+}
+
+TEST(ToRoman, Five){
+
+    ASSERT_EQ("V", toRoman(5));
+}
+
+TEST(ToRoman,Six){
+    ASSERT_EQ("VI", toRoman(6));
+}
+
+TEST(ToRoman,Ten){
+    ASSERT_EQ("X", toRoman(10));
+}
+
+TEST(ToRoman,twenty){
+    ASSERT_EQ("XX", toRoman(20));
+}
